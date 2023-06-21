832d8bd1dcbb688e8cea6551456ad98b8f114f1b
sohostory <sohostory@gmail.com>
2023-06-21 11:00:30 -0500

    setup sequelize

---
+++ server/util/database.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+const { CONNECTION_STRING } = process.env;
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = {
+  sequelize,
+};
