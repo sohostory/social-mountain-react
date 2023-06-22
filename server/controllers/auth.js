36c8b5ba17e73f9cc4cf9a921f015afd77fc5312
sohostory <sohostory@gmail.com>
2023-06-22 11:24:07 -0500

    part 4-1 completed - added register endpoint controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,50 @@
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+import bcrypt from "bcryptjs/dist/bcrypt";
+import { User } from "../models/user";
+
+const { SECRET } = process.env;
+
+const createToken = (username, id) => {
+  return jwt.sign({ username, id }, SECRET, { expiresIn: "2 Days" });
+};
+
 module.exports = {
-  register: (req, res) => {
-    console.log("register");
+  register: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      let foundUser = await User.findOne({ where: { username: username } });
+
+      if (foundUser) {
+        res.status(400).send("cannot create user");
+      } else {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+        const newUser = await User.create({
+          username: username,
+          hashedPass: hash,
+        });
+
+        const token = createToken(
+          newUser.dataValues.username,
+          newUser.dataValues.id
+        );
+        const exp = Date.now() + 1000 * 60 * 60 * 48;
+
+        res.status(200).send({
+          username: newUser.dataValues.username,
+          userId: newUser.dataValues.id,
+          token,
+          exp,
+        });
+      }
+    } catch (err) {
+      console.log("error in register", err);
+      res.sendStatus(400);
+    }
   },
   login: (req, res) => {
     console.log("login");
